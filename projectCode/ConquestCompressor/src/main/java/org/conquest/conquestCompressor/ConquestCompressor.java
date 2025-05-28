95f4ba6823b4061e193f1f74f056bea54fadc2fc
Joseph Sheets <JSheets1@IAState.edu>
2025-05-28 03:10:15 -0500

    basicas

---
+++ projectCode/ConquestCompressor/src/main/java/org/conquest/conquestCompressor/ConquestCompressor.java
@@ -0,0 +1,17 @@
+package org.conquest.conquestCompressor;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ConquestCompressor extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
